1e3045db9315803a14f226a9a102a938f88054d8
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2021-02-14 12:30:34 -0500

    just a test

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -1,5 +1,8 @@
 import math
+import random
+
 import agent
+
 
 ###########################
 # Alpha-Beta Search Agent #
@@ -16,6 +19,76 @@ class AlphaBetaAgent(agent.Agent):
         super().__init__(name)
         # Max search depth
         self.max_depth = max_depth
+        self.table = dict()
+        self.table_len = 10000
+        self.hits = 0
+
+    def isConnectable(self, brd):
+        pass
+
+    def terminalTest(self, brd):
+        return brd.get_outcome() != 0
+
+    def staticEval(self, brd):
+        board = brd.copy().board
+        for i in range(len(board)):
+            board[i] = tuple(board[i])
+        board = tuple(board)
+        print(board)
+        brd_hash = hash(board)
+        print("Hash: ", hash(board))
+        print("Length: ", len(self.table))
+        print("Hits: ", self.hits)
+
+        if hash(board) in self.table:
+            value = self.table[brd_hash]
+            self.hits += 1
+            return value
+        else:
+            # magic stuff
+            value = int(random.randrange(1000))
+            self.table[brd_hash] = value
+            if len(self.table) > self.table_len:
+                iterator = iter(self.table)
+                del self.table[next(iterator)]
+            return value
+
+    def minimax(self, brd, col, depth, alpha, beta, maximizingPlayer): # First
+        if depth == 0 or self.terminalTest(brd):
+            return self.staticEval(brd), col
+
+        if maximizingPlayer:
+            maxEval = -99999999
+            best_action = col
+            for child in self.get_successors(brd):
+                evalu, column = self.minimax(child[0], child[1], depth-1, alpha, beta, not(maximizingPlayer))
+                #maxEval = max(maxEval, evalu)
+                if max(maxEval, evalu) > maxEval:
+                    maxEval = evalu
+                    best_action = child[1]
+
+                alpha = max(alpha, evalu)
+                if beta <= alpha:
+                    print("Pruned")
+                    break
+            return maxEval, best_action
+
+        else:
+            minEval = 99999999
+            best_action = col
+            for child in self.get_successors(brd):
+                evalu, column = self.minimax(child[0], child[1], depth-1, alpha, beta, not(maximizingPlayer))
+                #minEval = min(minEval, evalu)
+                if min(minEval, evalu) < minEval:
+                    minEval = evalu
+                    best_action = child[1]
+
+                beta = min(beta, evalu)
+                if beta <= alpha:
+                    print("Pruned")
+                    break
+
+            return minEval, best_action
 
     # Pick a column.
     #
@@ -26,6 +99,39 @@ class AlphaBetaAgent(agent.Agent):
     def go(self, brd):
         """Search for the best move (choice of column for the token)"""
         # Your code here
+        '''board = brd.copy().board  # make copy of input board
+        table_len = 3
+        for i in range(len(board)):
+            board[i] = tuple(board[i])
+        print(tuple(board))
+        brd_hash = hash(tuple(board))
+        print("Hash: ", hash(tuple(board)))
+        self.table[brd_hash] = brd_hash
+        if len(self.table) > table_len:
+            iterator = iter(self.table)
+            del self.table[next(iterator)]
+
+        for i in range(4,1,-1):
+            print(i)
+        moves = [[],[],[]]
+        for i in range(4,1, -1): # from 4 to 2 starting at 4
+            brd.n = i
+            counter = 0
+            new = []
+            for x in range(brd.w):
+                for y in range(brd.h):
+                    if (brd.board[y][x] != 0) and brd.is_any_line_at(x,y):
+                        new.append([x,y])
+            moves[4-i] = new
+        print(moves)'''
+
+        value, move = self.minimax(brd, -1, 4, -99999999, 99999999, True)
+        print("Results!: ", value, move)
+
+        if 3 in brd.free_cols():
+            return 3
+        else:
+            return move
 
     # Get the successors of the given board.
     #
@@ -49,5 +155,5 @@ class AlphaBetaAgent(agent.Agent):
             # (This internally changes nb.player, check the method definition!)
             nb.add_token(col)
             # Add board to list of successors
-            succ.append((nb,col))
+            succ.append((nb, col))
         return succ
