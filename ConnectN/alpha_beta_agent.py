5bfe951c0be7a94ca38e45047f75ea253390b2ba
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2021-02-17 15:10:48 -0500

    set up nav and heuristic

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -29,33 +29,117 @@ class AlphaBetaAgent(agent.Agent):
     def terminalTest(self, brd):
         return brd.get_outcome() != 0
 
-    def staticEval(self, brd):
+    def heuristic(self, brd, col, maximizingPlayer):
+        #print("calling heuristic...")
+
+        def countLineValue(x, y, dx, dy):
+            """Return True if a line of identical tokens exists starting at (x,y) in direction (dx,dy)"""
+            # Avoid out-of-bounds errors
+            '''if ((x + (self.n-1) * dx >= self.w) or
+                    (y + (self.n-1) * dy < 0) or (y + (self.n-1) * dy >= self.h)):
+                return False'''
+            # Get token at (x,y)
+
+            t = brd.board[y][x]
+            # Go through elements
+            myPieces = 1
+            holes = 0
+            savedrow = 0
+            for i in range(1, brd.n):
+                inarow = 0
+                if ((y + i * dy) >= brd.h) or ((x + i * dx) >= brd.w):
+                    break
+                elif brd.board[y + i*dy][x + i*dx] != t and brd.board[y + i*dy][x + i*dx] != 0:
+                    break
+                elif brd.board[y + i*dy][x + i*dx] == t:
+                    inarow += 1
+                    myPieces += 1
+                elif brd.board[y + i*dy][x + i*dx] == 0:
+                    if inarow > savedrow:
+                        savedrow = inarow
+                    inarow = 0
+                    holes += 1
+
+            for i in range(1, brd.n):
+                inarow = 0
+                if ((y - i * dy) < 0) or ((x - i * dx) < 0):
+                    break
+                elif brd.board[y - i*dy][x - i*dx] != t and brd.board[y - i*dy][x - i*dx] != 0:
+                    break
+                elif brd.board[y - i*dy][x - i*dx] == t:
+                    inarow += 1
+                    myPieces += 1
+                elif brd.board[y - i*dy][x - i*dx] == 0:
+                    if inarow > savedrow:
+                        savedrow += inarow
+                    inarow = 0
+                    holes += 1
+
+            value = 0
+            if myPieces < brd.n and holes == 0:
+                return 10
+            elif holes + myPieces < brd.n:
+                return 10
+            elif holes == 0:
+                value = 100**brd.n
+            elif abs(dx) != abs(dy):
+                value = 10**(myPieces*savedrow)
+            else:
+                value = 5**(myPieces*savedrow)
+
+            return value
+
+        y = 0
+        for i in range(brd.h-1, -1, -1):
+            if brd.board[i][col] != 0:
+                y = i
+
+        horiz_value = countLineValue(col, y, 1, 0)
+        vert_value = countLineValue(col, y, 0, 1)
+        diag_pos_value = countLineValue(col, y, 1, 1)
+        diag_neg_value = countLineValue(col, y, 1, -1)
+
+        value = horiz_value + vert_value + diag_pos_value + diag_neg_value
+
+        #print("Calculated Value: ", value)
+        if maximizingPlayer:
+            return value
+        else:
+            return -1*value
+
+
+    def staticEval(self, brd, col, maximizingPlayer):
         board = brd.copy().board
-        for i in range(len(board)):
+        '''for i in range(len(board)):
             board[i] = tuple(board[i])
         board = tuple(board)
-        print(board)
+        #print(board)
         brd_hash = hash(board)
         print("Hash: ", hash(board))
         print("Length: ", len(self.table))
-        print("Hits: ", self.hits)
+        print("Hits: ", self.hits)'''
 
-        if hash(board) in self.table:
+        '''if hash(board) in self.table:
             value = self.table[brd_hash]
             self.hits += 1
-            return value
-        else:
-            # magic stuff
-            value = int(random.randrange(1000))
-            self.table[brd_hash] = value
-            if len(self.table) > self.table_len:
-                iterator = iter(self.table)
-                del self.table[next(iterator)]
-            return value
+            #print("Value: ", value)
+            if maximizingPlayer:
+                return value
+            else:
+                return -1*value
+        else:'''
+        # magic stuff
+        value = self.heuristic(brd, col, maximizingPlayer)
+        '''self.table[brd_hash] = value
+        if len(self.table) > self.table_len:
+            iterator = iter(self.table)
+            del self.table[next(iterator)]'''
+        #print("Value: ", value)
+        return value
 
     def minimax(self, brd, col, depth, alpha, beta, maximizingPlayer): # First
         if depth == 0 or self.terminalTest(brd):
-            return self.staticEval(brd), col
+            return self.staticEval(brd, col, maximizingPlayer), col
 
         if maximizingPlayer:
             maxEval = -99999999
@@ -65,11 +149,11 @@ class AlphaBetaAgent(agent.Agent):
                 #maxEval = max(maxEval, evalu)
                 if max(maxEval, evalu) > maxEval:
                     maxEval = evalu
-                    best_action = child[1]
+                    best_action = column
 
                 alpha = max(alpha, evalu)
                 if beta <= alpha:
-                    print("Pruned")
+                    #print("Pruned")
                     break
             return maxEval, best_action
 
@@ -81,11 +165,11 @@ class AlphaBetaAgent(agent.Agent):
                 #minEval = min(minEval, evalu)
                 if min(minEval, evalu) < minEval:
                     minEval = evalu
-                    best_action = child[1]
+                    best_action = column
 
                 beta = min(beta, evalu)
                 if beta <= alpha:
-                    print("Pruned")
+                    #print("Pruned")
                     break
 
             return minEval, best_action
@@ -99,39 +183,10 @@ class AlphaBetaAgent(agent.Agent):
     def go(self, brd):
         """Search for the best move (choice of column for the token)"""
         # Your code here
-        '''board = brd.copy().board  # make copy of input board
-        table_len = 3
-        for i in range(len(board)):
-            board[i] = tuple(board[i])
-        print(tuple(board))
-        brd_hash = hash(tuple(board))
-        print("Hash: ", hash(tuple(board)))
-        self.table[brd_hash] = brd_hash
-        if len(self.table) > table_len:
-            iterator = iter(self.table)
-            del self.table[next(iterator)]
-
-        for i in range(4,1,-1):
-            print(i)
-        moves = [[],[],[]]
-        for i in range(4,1, -1): # from 4 to 2 starting at 4
-            brd.n = i
-            counter = 0
-            new = []
-            for x in range(brd.w):
-                for y in range(brd.h):
-                    if (brd.board[y][x] != 0) and brd.is_any_line_at(x,y):
-                        new.append([x,y])
-            moves[4-i] = new
-        print(moves)'''
-
-        value, move = self.minimax(brd, -1, 4, -99999999, 99999999, True)
+        value, move = self.minimax(brd, -1, self.max_depth, -99999999, 99999999, True)
         print("Results!: ", value, move)
 
-        if 3 in brd.free_cols():
-            return 3
-        else:
-            return move
+        return move
 
     # Get the successors of the given board.
     #
--- ConnectN/run.py
+++ ConnectN/run.py
@@ -4,7 +4,7 @@ import agent
 import alpha_beta_agent as aba
 
 # Set random seed for reproducibility
-random.seed(1)
+#random.seed(1)
 
 #
 # Random vs. Random
@@ -31,16 +31,16 @@ random.seed(1)
 #               6, # height
 #               4, # tokens in a row to win
 #               agent.RandomAgent("random"),        # player 1
-#               aba.AlphaBetaAgent("alphabeta", 4)) # player 2
+#               aba.AlphaBetaAgent("alphabeta", 6)) # player 2
 
 #
 # Human vs. AlphaBeta
 #
-g = game.Game(7, # width
-              6, # height
-              4, # tokens in a row to win
-              agent.InteractiveAgent("human"),    # player 1
-              aba.AlphaBetaAgent("alphabeta", 4)) # player 2
+# g = game.Game(7, # width
+#               6, # height
+#               4, # tokens in a row to win
+#               agent.InteractiveAgent("human"),    # player 1
+#               aba.AlphaBetaAgent("alphabeta", 4)) # player 2
 
 #
 # Human vs. Human
@@ -52,4 +52,21 @@ g = game.Game(7, # width
 #               agent.InteractiveAgent("human2"))   # player 2
 
 # Execute the game
-outcome = g.go()
+win = 0
+loss = 0
+tie = 0
+for i in range(0, 10):
+    g = game.Game(7, # width
+                  6, # height
+                  4, # tokens in a row to win
+                  agent.RandomAgent("random"),        # player 1
+                  aba.AlphaBetaAgent("alphabeta", 6)) # player 2
+    outcome = g.go()
+    if outcome == 2:
+        win += 1
+    elif outcome == 1:
+        loss += 1
+    else:
+        tie += 1
+
+print("Results: Wins: ", win, " Losses: ", loss, " Ties: ", tie, "Win pct: ", (win/(win+loss))*100)
